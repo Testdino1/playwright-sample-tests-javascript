6d1e9e327b6d1e02dc9fbad21828f201383e4618
kriti2710 <kriti@testdino.com>
2025-12-16 10:58:46 +0530

    Update GitHub Actions workflow for test execution

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -33,7 +33,7 @@ test('Verify that user can login and logout successfully', {tag: '@chromium'}, a
   await logout();
 });
 
-test('Verify that all the navbar are working properly', {tag: '@chromium'}, async () => {
+test('Verify that all the navbar are working properly', {tag: '@webkit'}, async () => {
     await login();
     await allPages.homePage.clickBackToHomeButton();
     // await allPages.homePage.assertHomePage();
